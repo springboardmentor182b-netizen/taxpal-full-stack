4be5d2682c3470d86504a7745d83e1680846642d
Sreeja0607 <orugantikumaraswamy321@gmail.com>
2025-09-16 17:49:16 +0530

    Add files via upload

---
+++ taxpal-dashboard/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
