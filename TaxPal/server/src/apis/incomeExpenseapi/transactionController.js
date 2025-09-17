ac22bc5b4ebbdc3662a1f06ac6f46e2b29ca0529
Akshaya Somu <akshayasomu2005@gmail.com>
2025-09-17 20:39:55 +0530

    Update transactionController.js

--- TaxPal/server/src/apis/incomeExpenseapi/transactionController.js
+++ TaxPal/server/src/apis/incomeExpenseapi/transactionController.js
@@ -1,19 +1,19 @@
 const Transaction = require("../TransactionModel");
 
+
 exports.createTransaction = async (req, res) => {
   try {
-    const tx = await Transaction.create({ ...req.body, user: req.user.id });
+    const tx = await Transaction.create({ ...req.body, userId: req.user.id }); // use userId
     res.status(201).json(tx);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
+
 exports.getTransactions = async (req, res) => {
   try {
-    const txs = await Transaction.find({ user: req.user.id }).sort({
-      date: -1,
-    });
+    const txs = await Transaction.find({ userId: req.user.id }).sort({ date: -1 }); // use userId
     res.json(txs);
   } catch (err) {
     res.status(500).json({ message: err.message });
