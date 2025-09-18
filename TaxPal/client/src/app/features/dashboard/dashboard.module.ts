8ec6288c3c84c5b7ca6aa27501664c375f494b10
Somnath <somnathuma71@gmail.com>
2025-09-19 00:53:10 +0530

    refactor: remove unused imports and declarations in user profile and dashboard modules

--- TaxPal/client/src/app/features/dashboard/dashboard.module.ts
+++ TaxPal/client/src/app/features/dashboard/dashboard.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule],
-  exports: [DashboardComponent]
+  imports: [CommonModule]
 })
 export class DashboardModule {}
