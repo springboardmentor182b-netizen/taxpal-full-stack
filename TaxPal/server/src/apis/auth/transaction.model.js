46f7c8080734bc9585746759140b8779de9b612a
vinothcj18 <vinothj2050@gmail.com>
2025-09-19 15:06:42 +0530

    Add files via upload

---
+++ TaxPal/server/src/apis/auth/transaction.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    type: { type: String, enum: ['income', 'expense'], required: true },
+    category: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
