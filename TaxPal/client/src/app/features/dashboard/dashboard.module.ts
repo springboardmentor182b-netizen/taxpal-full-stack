394b20675e8bd0a91ba603ad180b4748dff27d62
Sreeja0607 <orugantikumaraswamy321@gmail.com>
2025-09-21 09:01:14 +0000

    Group-D/feature/APi

--- TaxPal/client/src/app/features/dashboard/dashboard.module.ts
+++ TaxPal/client/src/app/features/dashboard/dashboard.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
+// import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [],
   imports: [CommonModule],
-  exports: [DashboardComponent]
+  exports: []
 })
 export class DashboardModule {}
