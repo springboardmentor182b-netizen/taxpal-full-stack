ed8045ffd3bf8add69d5caada6eb244c0fc7b4d4
Sreeja0607 <orugantikumaraswamy321@gmail.com>
2025-10-01 09:32:08 +0530

    Update server.js

--- TaxPal/server/src/server.js
+++ TaxPal/server/src/server.js
@@ -33,6 +33,8 @@ app.use('/api/user', userRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/auth', authRoutes);
 
+
+
 // Root route
 app.get('/', (req, res) => {
     res.send('Welcome to TaxPal API 🚀');
@@ -43,3 +45,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
