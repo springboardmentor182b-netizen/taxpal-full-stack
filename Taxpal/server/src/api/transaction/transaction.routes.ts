ae1a03a5ffa40bdf2e31adda5282f313af336ec6
Ayushi <145868166+08Ayushi@users.noreply.github.com>
2025-10-27 17:34:06 +0530

    Added delete button for single transaction

--- Taxpal/server/src/api/transaction/transaction.routes.ts
+++ Taxpal/server/src/api/transaction/transaction.routes.ts
@@ -13,25 +13,13 @@ import { handleValidationErrors } from '../../utils/validators/dashboardValidati
 
 const r = Router();
 
-// all transaction routes require auth
 r.use(authenticateToken);
 
-// List transactions (supports filters via query)
 r.get('/', getTransactions);
-
-// Get one by id (needed by Angular getTransaction(id))
 r.get('/:id', getTransactionById);
-
-// Create
 r.post('/', validateTransaction, handleValidationErrors, createTransaction);
-
-// Update one
 r.put('/:id', validateTransaction, handleValidationErrors, updateTransaction);
-
-// Delete ALL for current user
 r.delete('/', deleteAllTransactions);
-
-// Delete one by id
 r.delete('/:id', deleteTransaction);
 
 export default r;
