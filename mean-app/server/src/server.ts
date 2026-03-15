f085e32bed08a7ece17aefba7b1e0ffe8312e083
Harshitha-965 <harshithakg09@gmail.com>
2026-03-15 22:40:45 +0530

    Update server.ts

--- mean-app/server/src/server.ts
+++ mean-app/server/src/server.ts
@@ -3,8 +3,8 @@ import app from './app.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
+//const PORT = process.env.PORT || 5000;
+const PORT=4000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
